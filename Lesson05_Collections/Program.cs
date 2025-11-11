4b1ec8924fa80bef1bd5d0c27ea765ba5dcd40ca
Owner <stam1@gmail.com>
2025-11-11 08:35:51 +0200

    Update Delegates

---
+++ Lesson05_Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.IO.IsolatedStorage;
+
+namespace Lesson05_Collections
+{
+	internal class Program:object
+	{
+		static void Main(string[] args)
+		{
+			var x=new Queue<int>();
+			var y = 5;
+			var r = "Tal";
+			List<int> ls = new List<int>();
+			ls.Add(6);
+			ls.Add(9);
+			ls.Add(27);
+			Console.WriteLine(ls[2]);
+			Console.WriteLine(ls[0]+5);
+			ls[1] = ls[0] + 5;
+			foreach(var i in ls)
+				Console.WriteLine(i);
+
+			Console.WriteLine(ls.Count);
+			ls.Remove(1);
+			ls.Contains(1);
+			Console.WriteLine(ls.Count);
+
+			Dictionary<int, string> codes = new Dictionary<int, string>();
+		codes.Add(44, "England");
+			codes.Add(44, "Uganda");
+
+			string country = codes[44];
+
+			Dictionary<string, string> colors;
+
+		}
+
+
+	}
+}
---
+++ Lesson_05_Delegates/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Lesson_05_Delegates
+{
+	public delegate bool ValidateRule(int num);
+	public delegate void PrintMessage(string m);
+	internal class Program
+	{
+
+		public static List<int> FilterByCondition(List<int> lst, ValidateRule r)
+		{
+			List<int> result = new List<int>();
+			foreach (int i in lst)
+			{
+				if(r(i))
+					result.Add(i);
+			}
+			return result;
+		}
+
+		public static bool IsEven(int n)
+		{
+			return n % 2 == 0;
+		}
+		public static List<int> OnlyEven(List<int> lst)
+		{
+			List<int> result = new List<int>();
+			foreach(var i in lst)
+							{
+				if(IsEven(i))
+					result.Add(i);
+			}
+			return result;
+		}
+
+		public static bool IsBiggerThan5(int n)
+		{
+			return n > 5;
+		}
+		public static List<int> BiggerThan5(List<int> lst)
+		{
+			List<int> result = new List<int>();
+			foreach (var i in lst)
+			{
+				if (IsBiggerThan5(i) )
+					result.Add(i);
+			}
+			return result;
+		}
+
+		public static void MyMessage(string m)
+		{
+			Console.WriteLine(m);
+		}
+			static void Main(string[] args)
+		{
+			PrintMessage msg = MyMessage;
+			msg("Go Home");
+			List<int> ls=[5,12,3,27,2];
+			ValidateRule valid = IsEven;
+		
+			var result= FilterByCondition(ls, valid);
+			foreach(var i in result)
+			{
+				Console.WriteLine(i);
+			}
+			//valid = IsBiggerThan5;
+		    result = FilterByCondition(ls, IsBiggerThan5);
+			foreach (var i in result)
+			{
+				Console.WriteLine(i);
+			}
+
+
+		}
+	}
+}
